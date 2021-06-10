b5d0fee825bbef040cef87c53bb2e3a9ab2c4a9f
zigart <agustinzigart@gmail.com>
2021-06-10 20:41:29 -0300

    Cambio en ruta de rxjs

--- proyecto-angular/src/app/services/project.service.ts
+++ proyecto-angular/src/app/services/project.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 import {Project} from '../models/project';
 import {global} from './global';
 
